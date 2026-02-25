19f0e25eddc599a40be4455435e48c90a83c7590
Marta Dzelme <s23dzelmart@vea.lv>
2026-02-25 11:17:24 +0200

    3.dala

--- src/model/Student.java
+++ src/model/Student.java
@@ -6,7 +6,10 @@ public class Student {
 	private long studId;
 	private String name;
 	private String surname;
-	private String peronCode;
+	private String personCode;
+	
+	// Paligmainigais kam nevajag ne set, ne get
+	private static long counter = 0;
 	
 	// 2. get funkcijas
 	public long getStudId() {
@@ -22,12 +25,38 @@ public class Student {
 	}
 	
 	public String personCode() {
-		return peronCode;
+		return personCode;
 	}
 	
 	// 3. set funkcijas
+	public void setTudId() {
+		studId = counter;
+		counter++;
+	}
 	
+	public void setName(String inputName) {
+		if((inputName != null) && (inputName.matches("[A-Z]{1}[a-z]{2,15}([ ][1][A-Z]{1}[a-z]{2,15})?"))) {
+			name = inputName;
+		}else {
+			name = "Unknown";
+		}
+	}
 	
+	public void setSurname(String inputSurname) {
+		if((inputSurname != null) && (inputSurname.matches("[A-Z]{1}[a-z]{2,15}([-][1][A-Z]{1}[a-z]{2,15})?"))) {
+			surname = inputSurname;
+		}else {
+			surname = "Unknown";
+		}
+	}
+	
+	public void setPersonCode(String inputPersonCode) {
+		if((inputPersonCode != null) && inputPersonCode.matches("[0-9]{6}[-][0-9]{5}")) {
+			personCode = inputPersonCode;
+		}else {
+			personCode = "Unknown";
+		}
+	}
 	
 	
 	
