c19159faaef7505a1085908d6d74b07f5208e5d0
Marta Dzelme <s23dzelmart@vea.lv>
2026-02-25 08:30:04 +0200

    get funkcijas

--- src/model/Student.java
+++ src/model/Student.java
@@ -2,9 +2,44 @@ package model;
 
 public class Student {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	// 1. mainigie
+	private long studId;
+	private String name;
+	private String surname;
+	private String peronCode;
+	
+	// 2. get funkcijas
+	public long getStudId() {
+		return studId;
 	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getSurname() {
+		return surname;
+	}
+	
+	public String personCode() {
+		return peronCode;
+	}
+	
+	// 3. set funkcijas
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	// 4.1. bezargumenta konstruktors
+	// 4.2. argumenta konstruktors
+	// 5. toString funkcija
+	// 6. Parejas pec nepieciesamibas
 
 }
