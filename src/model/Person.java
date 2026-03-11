b48bdb9a74c72f47521fb8d7f523a63f2a592778
Marta Dzelme <s23dzelmart@vea.lv>
2026-03-11 09:12:50 +0200

    person added

--- src/model/Person.java
+++ src/model/Person.java
@@ -3,17 +3,13 @@ package model;
 public class Person {
 
 	// 1. mainigie
-	private long studId;
 	private String name;
 	private String surname;
 	private String personCode;
-	
-	private long counter = 5000;
+
 
 	// 2. get funkcijas
-	public long getStudId() {
-		return studId;
-	}
+
 
 	public String getName() {
 		return name;
@@ -28,10 +24,6 @@ public class Person {
 	}
 
 	// 3. set funkcijas
-	public void setStudId() {
-		studId = counter;
-		counter++;
-	}
 
 	public void setName(String inputName) {
 		if((inputName != null) && (inputName.matches("[A-Z]{1}[a-z]{2,15}([ ]{1}[A-Z]{1}[a-z]{2,15})?"))) {
