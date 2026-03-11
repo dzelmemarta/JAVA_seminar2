7aa938bf1546521c5cab0fd71fc7a53034aee5dd
Marta Dzelme <s23dzelmart@vea.lv>
2026-03-11 09:37:28 +0200

    changed prof o use person

--- src/model/Professor.java
+++ src/model/Professor.java
@@ -2,13 +2,10 @@ package model;
 
 import model.enums.ProfDegree;
 
-public class Professor {
+public class Professor extends Person {
 	
 	// 1. mainigie
 	private long profId;
-	private String name;
-	private String surname;
-	private String persCode;
 
 	private ProfDegree degree;
 	
@@ -18,42 +15,15 @@ public class Professor {
 	public long getProfId() {
 		return profId;
 	}
-	public String getName() {
-		return name;
-	}
-	public String getSurname() {
-		return surname;
-	}
+
 	public ProfDegree getDegree() {
 		return degree;
 	}
-	public String getPersCode() {
-		return persCode;
-	}
-	
 	// 3. setters
-
-	public void setName(String name) {
-		if((name != null) && (name.matches("[A-Z]{1}[a-z]{2,15}([ ]{1}[A-Z]{1}[a-z]{2,15})?"))) {
-			this.name = name;
-		}
-	}
-	
-	public void setSurname(String surname) {
-		if((surname != null) && (surname.matches("[A-Z]{1}[a-z]{2,15}([-]{1}[A-Z]{1}[a-z]{2,15})?"))) {
-			this.surname = surname;
-		}
-	}
 	
 	public void setProfId() {
 		profId = counter;
 		counter++;
-	}
-	
-	public void setPersCode(String persCode) {
-		if((persCode != null) && persCode.matches("[0-9]{6}[-][0-9]{5}")) {
-			this.persCode = persCode;
-		}
 	}
 	
 	public void setDegree(ProfDegree inputDegree) {
@@ -67,9 +37,8 @@ public class Professor {
 
 	// 4.1. bezargumenta konstruktors
 	public Professor() {
+		super("Karina", "Skirmante", "121212-56789");
 		setProfId();
-		setName("Marks");
-		setSurname("Ozolins");
 		setDegree(ProfDegree.bachelor);
 	}
 
@@ -77,17 +46,15 @@ public class Professor {
 
 
 	public Professor(String inputName, String inputSurname, ProfDegree inputDegree, String persCode) {
+		super(inputName, inputSurname, persCode);
 		setProfId();
-		setName(inputName);
-		setSurname(inputSurname);
-		setPersCode(persCode);
 		setDegree(inputDegree);
 	}
 
 	// 5. toString funkcija
 	@Override	
 	public String toString() {
-		String result = profId + ": " + name + " " + surname + "(" + degree + ")";
+		String result = profId + ": " + name + " " + surname + "(" + degree + ") " + personCode;
 		return result;
 	}
 		
